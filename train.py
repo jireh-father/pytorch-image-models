c55466616626d94ed8ceba3d574f75c9bceade23
ilseo <ilseo@gmarket.com>
2024-04-05 10:21:13 +0900

    add metrics

--- train.py
+++ train.py
@@ -44,6 +44,7 @@ try:
     from apex import amp
     from apex.parallel import DistributedDataParallel as ApexDDP
     from apex.parallel import convert_syncbn_model
+
     has_apex = True
 except ImportError:
     has_apex = False
@@ -57,18 +58,19 @@ except AttributeError:
 
 try:
     import wandb
+
     has_wandb = True
 except ImportError:
     has_wandb = False
 
 try:
     from functorch.compile import memory_efficient_fusion
+
     has_functorch = True
 except ImportError as e:
     has_functorch = False
 
 has_compile = hasattr(torch, 'compile')
-
 
 _logger = logging.getLogger('train')
 
@@ -77,7 +79,6 @@ _logger = logging.getLogger('train')
 config_parser = parser = argparse.ArgumentParser(description='Training Config', add_help=False)
 parser.add_argument('-c', '--config', default='', type=str, metavar='FILE',
                     help='YAML config file specifying default arguments')
-
 
 parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
 
@@ -172,7 +173,7 @@ scripting_group.add_argument('--torchcompile', nargs='?', type=str, default=None
 # Device & distributed
 group = parser.add_argument_group('Device parameters')
 group.add_argument('--device', default='cuda', type=str,
-                    help="Device (accelerator) to use.")
+                   help="Device (accelerator) to use.")
 group.add_argument('--amp', action='store_true', default=False,
                    help='use NVIDIA Apex AMP or Native AMP for mixed precision training')
 group.add_argument('--amp-dtype', default='float16', type=str,
@@ -1195,12 +1196,13 @@ def validate(
     confusion_matrix = utils.get_confusion_matrix(total_preds, total_targets)
     classification_report = utils.get_classification_report(total_preds, total_targets)
     metrics = OrderedDict([('loss', losses_m.avg), ('top1', top1_m.avg), ('top5', top5_m.avg),
-                            ('f1', f1_m.avg), ('recall', recall_m.avg), ('precision', precision_m.avg),
-                            ('confusion_matrix', confusion_matrix), ('classification_report', classification_report)])
+                           ('f1', f1_m.avg), ('recall', recall_m.avg), ('precision', precision_m.avg),
+                           ('confusion_matrix', confusion_matrix), ('classification_report', classification_report)])
 
+    _logger.info(f'Validation results: {OrderedDict([('loss', losses_m.avg), ('top1', top1_m.avg), ('top5', top5_m.avg),
+                                                     ('f1', f1_m.avg), ('recall', recall_m.avg), ('precision', precision_m.avg)])}')
     _logger.info(f'Classification report: {classification_report}')
     _logger.info(f'Confusion matrix: {confusion_matrix}')
-    _logger.info(f'Validation results: {metrics}')
 
     return metrics
 
